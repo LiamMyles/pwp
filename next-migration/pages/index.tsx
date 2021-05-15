2eb82352556e69d7a7d366725aeadc5156249882
Liam Myles <liamthemonkey92@gmail.com>
2021-05-15 23:25:18 +0100

    Clean up project and move to strict typescript

--- next-migration/pages/index.tsx
+++ next-migration/pages/index.tsx
@@ -120,6 +120,12 @@ function App({}: AppProps) {
           content="Maths, Polygons and Adventures to be had"
         />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css"
+          integrity="sha512-NmLkDIU1C/C88wi324HBc+S2kLhi08PN5GDeUVVVC/BVt/9Izdsc9SVeVfA1UZbY3sHUlDSyRXhCzHfr6hmPPw=="
+          crossOrigin="anonymous"
+        />
       </Head>
       <StyledP5Canvas sketch={sketch} />
       <StyledSlider>
