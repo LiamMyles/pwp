f271fd7308ed7b984c814f881d2e6700e37b236d
Liam Myles <liamthemonkey92@gmail.com>
2021-05-15 23:52:27 +0100

    Move next.js to top level

--- src/components/P5Canvas.tsx
+++ src/components/P5Canvas.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef } from "react"
-import p5 from "p5"
 import type P5 from "p5"
 
 interface P5CanvasProps {
@@ -11,24 +10,31 @@ export const P5Canvas: React.FC<P5CanvasProps> = ({ sketch, className }) => {
   const divRef = useRef<HTMLDivElement>(null)
   const P5Ref = useRef<P5>()
 
-  useEffect(() => {
-    const node = divRef.current as HTMLDivElement
-    if (P5Ref.current !== undefined) {
-      P5Ref.current.remove()
-    }
-    //Add delay to help handle any excess renders
-    const delay = setTimeout(() => {
-      const p5Instance = new p5(sketch, node)
-      P5Ref.current = p5Instance
-    }, 100)
+  if (typeof window === "undefined") {
+    return <div ref={divRef} className={className} />
+  }
 
-    return () => {
-      clearTimeout(delay)
-      if (P5Ref?.current) {
-        const P5Instance = P5Ref.current as P5
-        P5Instance.remove()
+  useEffect(() => {
+    ;(async () => {
+      const p5 = (await import("p5")).default
+      const node = divRef.current as HTMLDivElement
+      if (P5Ref.current !== undefined) {
+        P5Ref.current.remove()
       }
-    }
+      //Add delay to help handle any excess renders
+      const delay = setTimeout(() => {
+        const p5Instance = new p5(sketch, node)
+        P5Ref.current = p5Instance
+      }, 100)
+
+      return () => {
+        clearTimeout(delay)
+        if (P5Ref?.current) {
+          const P5Instance = P5Ref.current as P5
+          P5Instance.remove()
+        }
+      }
+    })()
   }, [P5Ref, divRef, sketch])
 
   return <div ref={divRef} className={className} />
