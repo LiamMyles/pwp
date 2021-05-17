531b10e43e59d732f12240596c125879b574e014
Liam Myles <liamthemonkey92@gmail.com>
2021-05-17 23:49:50 +0100

    Move and fix polygon creation script

---
+++ src/calculations/getNGonVertices/getNGonVertices.ts
@@ -0,0 +1,20 @@
+import type { VerticesMatrix } from "Calculations/types"
+
+export function getNGonVertices(vertices: number): VerticesMatrix[] {
+  const twoPi = Math.PI * 2
+  const angleBetweenPoints = twoPi / vertices
+
+  let currentAngle = 0 - angleBetweenPoints
+
+  const initialVertices = [...Array(vertices)].map(() => {
+    currentAngle += angleBetweenPoints
+    const cos = Math.cos(currentAngle)
+    const sin = Math.sin(currentAngle)
+    const y = cos
+    const x = sin
+
+    return { x, y }
+  })
+
+  return initialVertices
+}
---
+++ src/calculations/types.tsx
@@ -0,0 +1,4 @@
+export interface VerticesMatrix {
+  x: number
+  y: number
+}
