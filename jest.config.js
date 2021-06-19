4dc3b81d36bf3a78e1c8e3e1524ddf44972a5674
Liam Myles <liamthemonkey92@gmail.com>
2021-06-19 23:29:10 +0100

    Add test coverage reports

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,26 @@ module.exports = {
     "^Calculations(.*)$": "<rootDir>/src/calculations$1",
     "^Src(.*)$": "<rootDir>/src$1",
   },
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/node_modules/**",
+    "!**/*.config.js",
+    "!**/.next/**/*",
+    "!**/_next/**/*",
+    "!**/coverage/**/*",
+    "!.eslintrc.js",
+    "!next-env.d.ts",
+    "!src/calculations/**/index.ts",
+    "!src/calculations/**/types.ts",
+    "!**/_app.tsx",
+    "!**/_document.tsx",
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 }
